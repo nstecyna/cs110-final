cc1d1b9dd1b9a925f0ce1f5c27823208a61b547e
nstecyna <31869551+nstecyna@users.noreply.github.com>
2017-11-20 13:45:45 -0500

    Updated Classes.py
    Added class descriptions.
    Removed whichPuzzle(). - We can initialize a function for each specific puzzle.

--- Classes.py
+++ Classes.py
@@ -22,11 +22,35 @@ class Key:
 		self.x = location[0]
 		self.y = location[1]
 
+import pygame
+
+class View:
+# initialize
+  # index telling which view it is 
+  # background image
+  # list of smaller images (to place in changes that show the changes from completing puzzles)
+  # Puzzles in this room, with 
+    # (x,y coordinates should be tuples, but can group the Puzzles and coordinates up in a list, dictionary, etc.)
+  # Keys in this room, with two sets of x,y coordinates
+  # Locks in this room, with two sets of x,y coordinates
+
+class Puzzle:
+# initialize
+  # index number
+  # two sets of x,y coordinates
+  # a function for that specific puzzle (we'll work on this after doing the structure)
+
+class Key:
+# initialize
+  # index number
+  # two sets of x,y coordinates
+
+# isUsed() - if used on the correct lock (matches index number?), should remove the key from the game, unlock the lock, and return true. else everything stays the same
 
 
+class Lock:
+# initialize
+  # index number
+  # two sets of x,y coordinates
 
-def whichPuzzle(number):
-	if (number == 1):
-		puzzle1()
-	elif ...
-
+# isOpened() - if the lock is opened, remove the lock from the game
