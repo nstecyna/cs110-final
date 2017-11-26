03fc2e17c9deecab149035931f5f5c1caedcd146
nstecyna <31869551+nstecyna@users.noreply.github.com>
2017-11-26 16:40:53 -0500

    Created Key.py

---
+++ Key.py
@@ -0,0 +1,7 @@
+class Key:
+# initialize
+  # index number
+  # two sets of x,y coordinates
+
+# isUsed() - if used on the correct lock (matches index number?),
+  # should remove the key from the game, unlock the lock, and return true. else everything stays the same
