c1603a9b41f71869e5a4a4d7a84afe64de9e3e16
nan3 <nan3@binghamton.edu>
2017-12-01 18:14:18 -0500

    Class files

--- button.py
+++ button.py
@@ -2,6 +2,7 @@ class Button:
 	def __init__(self, index, imageList, coordinates, correctNum, maxNum):
 		self.clickNum = 0
 		self.correctNum = 3
+		self.maxNum = 4
 	
 	def clicked(self):
 		if self.clickNum = maxNum:
@@ -9,3 +10,6 @@ class Button:
 		else:
 			self.clickNum += 1
 		self.image = self.imageList[self.clickNum]
+
+	def isCorrect():
+		return self.clickNum == self.correctNum
--- puzzle.py
+++ puzzle.py
@@ -1,8 +1,14 @@
 class Puzzle
-	def __init__(self, index, coordinates):
+	def __init__(self, index, coordinates, buttonList):
 		self.id = index
 		self.top_x = coordinates[0]
 		self.top_y = coordinates[1]
 		self.bottom_x = coordinates[2]
 		self.bottom_y = coordinates[3]
-		self.complete = False
+		self.buttonList = buttonList
+
+	def isComplete(self):
+		for button in buttonList:
+			if button.isCorrect() == False:
+				return False
+		return True
