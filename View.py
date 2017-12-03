95d0062aaf38923fa2056d9bfc2bc2ca298a9186
Nicholas Stecyna <31869551+nstecyna@users.noreply.github.com>
2017-12-03 15:18:50 -0500

    Delete View.py

--- View.py
+++
@@ -1,9 +0,0 @@
-class View:
-# initialize
-  # index telling which view it is 
-  # background image
-  # list of smaller images (to place in changes that show the changes from completing puzzles)
-  # Puzzles in this room, with 
-    # (x,y coordinates should be tuples, but can group the Puzzles and coordinates up in a list, dictionary, etc.)
-  # Keys in this room, with two sets of x,y coordinates
-  # Locks in this room, with two sets of x,y coordinates
