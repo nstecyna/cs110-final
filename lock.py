791f9feb88c24b04d812bfa3b3ff6d354362026b
Nicholas Stecyna <31869551+nstecyna@users.noreply.github.com>
2017-12-03 15:09:47 -0500

    Update lock.py

--- lock.py
+++ lock.py
@@ -1,10 +1,16 @@
-class Lock:
-	def __init__(self, index, coordinates, img_file):
-		self.id = index
-		self.image = pygame.image.load(img_file).convert.alpha()
-		self.top_x = coordinates[0]
-		self.top_y = coordinates[1]
-		self.bottom_x = coordinates[2]
-		self.bottom_y = coordinates[3]
-	
-		lock_rect = pygame.Rect(self.top_x, self.top_y, self.bottom_x, self.bottom_y)
+class Lock
+ 	def __init__(self, index, coordinates, image):
+  		self.id = index
+ 		self.image = image
+  		self.top_x = coordinates[0]
+  		self.top_y = coordinates[1]
+  		self.bottom_x = coordinates[2]
+  		self.bottom_y = coordinates[3]
+ 		self.locked = True
+ 
+ 	def isUnlocked(self):
+ 		self.locked = False
+ 			self.top_x = 0
+ 			self.top_y = 0
+ 			self.bottom_x = 0
+			self.bottom_y = 0
