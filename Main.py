860480e446a1a202819e2e9fb9f454660df597b4
Nicholas Stecyna <nstecyna@Nicholass-MacBook-Pro.local>
2017-12-03 12:11:57 -0500

    Updated Main.py and view.py

--- Main.py
+++ Main.py
@@ -4,16 +4,25 @@ import Puzzle
 import Key
 import Lock
 
-class Controller:	
+class Controller:
 
 	def __init__(self, width = 800, height = 500):
 		pygame.init()
 		self.width = width
 		self.height = height
 		self.screen = pygame.display.set_mode((self.width, self.height))
-		self.background = pygame.Surface(self.screen.get_size()).convert()
-		self.view1 = pygame.display.set_mode(width, height) #setting the window/display
-		self.puzzle1 = puzzle.Puzzle()
+
+		self.button1 = button.Button(1, [], (50,50,100,100), 3, 4)
+		self.puzzle1 = puzzle.Puzzle(1, (0,0,0,0), [button1])
+		self.view1 = view.View(1, 'lab1.png'v, [puzzle1], [], []])
+
+
+		self.background = pygame.image.load('lab1.png').convert()
+		self.background = pygame.transform.scale(self.background, (self.width, self.height))
+
+		#setting the window/display
+		self.currentView = view1 # how to tell which view we're using
+
 		self.key = key
 		self.lock = lock
 
@@ -22,17 +31,37 @@ class Controller:
 		self.key1_rect = pygame.Rect(top_x, top_y, bottom_x, bottom_y)
 		self.puzzle1_rect = pygame.Rect(top_x, top_y, bottom_x, bottom_y)
 		self.lock1_rect = pygame.Rect(top_x, top_y, bottom_x, bottom_y)
- 		self.lock1_rectzoom = pygame.Rect(top_x, top_y, bottom_x, bottom_y)		
-		
+ 		self.lock1_rectzoom = pygame.Rect(top_x, top_y, bottom_x, bottom_y)
+
+		self.viewList = [view1]
+
 
 	def mainLoop(self):
 		#refreshes the screen
 		#check if something's click, define what that click does
-		
+
 		for event in pygame.event.get():
 
 			if(event.type == pygame.MOUSEBUTTONDOWN and event.button == 1):
 				pos = self.position.copy()
+
+				if rightside_rect1.collidepoint(event.pos):
+					for view in self.viewList
+						if view.id == self.currentView.id + 1:
+							view = pygame.display.set_mode((self.width, self.height))
+
+
+				if leftside_rect1.collidepoint(event.pos):
+					for view in self.viewList
+						if view.id == self.currentView.id - 1:
+							view = pygame.display.set_mode((self.width, self.height))
+
+
+
+
+
+
+
 
 
 				############ view1 ###########
@@ -41,24 +70,24 @@ class Controller:
 					self.view2 = pygame.display.set_mode((self.width, self.height))
 
 				if leftside_rect1.collidepoint(event.pos):
-					self.view4 = pygame.display.set_mode((self.width, self.height))					
-					
+					self.view4 = pygame.display.set_mode((self.width, self.height))
+
 				#may literally zoom in instead of moving to a new view
 					#"zoom" in (basically new view)
-										
+
 					self.view5 = pygame.display.set_mode((self.width, self.height))
 
-				if puzzle1_rect.collidepoint(event.pos): 
-					#"zoom" in	
+				if puzzle1_rect.collidepoint(event.pos):
+					#"zoom" in
 					self.view6 = pygame.display.set_mode((self.width, self.height))
-			
+
 				if lock1_rect.collidepoint(event.pos):
 
 					self.view7 = pygame.display.set_mode((self.width, self.height))
 
 				if(lock1_rectzoom.collidepoint(event.pos) and key1_inv):
-					Lock.unlock()					
-			
+					Lock.unlock()
+
 
 				if key1_rect.collidepoint(event.pos):
 					Key.moveKey()
@@ -68,17 +97,17 @@ class Controller:
 					self.view3 = pygame.display.set_mode((self.width, self.height))
 
 				if leftside_rect2.collidepoint(event.pos):
-					self.view1 = pygame.display.set_mode((self.width, self.height))	
+					self.view1 = pygame.display.set_mode((self.width, self.height))
 
 				if key2_rect.collidepoint(event.pos):
 					#"zoom" in (basically new view)
-										
+
 					self.view8 = pygame.display.set_mode((self.width, self.height))
 
 				if puzzle2_rect.collidepoint(event.pos):
-					#"zoom" in	
+					#"zoom" in
 					self.view9 = pygame.display.set_mode((self.width, self.height))
-			
+
 				if lock2_rect.collidepoint(event.pos):
 
 					self.view10 = pygame.display.set_mode((self.width, self.height))
@@ -86,22 +115,22 @@ class Controller:
 
 				if(lock2_rectzoom.collidepoint(event.pos) and key2_inv):
 					Lock.unlock()
-					
+
 				############ view3 ############
-				
+
 				if rightside_rect3.collidepoint(event.pos):
 					self.view2 = pygame.display.set_mode((self.width, self.height))
 
 				if leftside_rect3.collidepoint(event.pos):
-					self.view4 = pygame.display.set_mode((self.width, self.height))	
+					self.view4 = pygame.display.set_mode((self.width, self.height))
 
 				if key2_rect.collidepoint(event.pos):
 					#"zoom" in (basically new view)
-										
+
 	                 	if puzzle2_rect.collidepoint(event.pos):
-					#"zoom" in 
+					#"zoom" in
 					self.view9 = pygame.display.set_mode((self.width, self.height))
-			
+
 				if lock2_rect.collidepoint(event.pos):
 
 					self.view10 = pygame.display.set_mode((self.width, self.height))
@@ -113,7 +142,7 @@ class Controller:
 				############# view4 ############
 
 
-		
+
 		#Check if puzzled are correct
 		puzzles = [puzzle1, puzzle2, puzzle3, puzzle4]
 		for puzzle in puzzles:
@@ -123,11 +152,10 @@ class Controller:
 				completion = True
 				final_completion += 1
 		if final_completion = 4 #number of total puzzles, may not need this if it just is done in order.. but more if statements for combined puzzles if in order
-		
-		
+
+
 def main():
 	main_program = Controller()
 	main_program.mainLoop()
 
 main()
-	
--- view.py
+++ view.py
@@ -1,19 +1,9 @@
 import pygame
 
 class View
-	def __init__(self, index, imagelist, puzzlelist, keylist, locklist):
+	def __init__(self, index, background, puzzlelist, keylist, locklist):
 		self.id = index
-		self.background = pygame.image.load(img_file).convert_alpha()
-		self.imagelist = imagelist
+		self.background = background
 		self.puzzle = puzzlelist
 		self.key = keylist
 		self.lock = locklist
-	def display_room1(self, #instance_variables):
-		#images/room1.png
-
-
-
-
-
-
-
