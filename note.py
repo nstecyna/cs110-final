c87763cd3fab6368967f8bc072f54cdf3ab440e6
koneill7 <33330281+koneill7@users.noreply.github.com>
2017-12-07 22:57:11 -0500

    Update note.py

--- note.py
+++ note.py
@@ -1,5 +1,10 @@
 class Note:
 	def __init__(self, index, image, coordinates, size):
+		"""
+		Creates a note object, assigns an index value to it, takes image from image file, sets its coordinates and dimensions
+		ParamList: self (self), index (int), image (string), coordinates (tuple), size (list)
+		Returns: none
+		"""
 		self.id = index
 		self.image = image
 		self.top_x = coordinates[0]
