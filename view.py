9d056dc50ca81429af5710ccfed94796662a1c3c
koneill7 <33330281+koneill7@users.noreply.github.com>
2017-12-07 23:06:52 -0500

    Update view.py

--- view.py
+++ view.py
@@ -2,6 +2,11 @@ import pygame
 
 class View:
 	def __init__(self, index, background, coordinates, size, viewlist, puzzlelist, keylist, locklist, notelist):
+		"""
+		Creates view object, assigns an index value to it, sets background from background image, sets its coordinates and dimensions, stores smaller views, puzzles, keys, locks, and notes in each of the different views from lists
+		ParamList: self (self), index (int), background (string), coordinates (tuple), size (list), viewlist (list), puzzlelist (list), keylist (list), locklist (list), notelist (list)
+		Returns: none
+		"""
 		self.id = index
 		self.background = background
 		self.top_x = coordinates[0]
