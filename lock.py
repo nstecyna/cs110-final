02f31b7b8001ece2441968c640f4f65930673b5c
koneill7 <33330281+koneill7@users.noreply.github.com>
2017-12-07 22:54:51 -0500

    Update lock.py

--- lock.py
+++ lock.py
@@ -1,5 +1,10 @@
 class Lock:
 	def __init__(self, index, imagefile, coordinates, size):
+		"""
+		Creates a lock object, assigns an index value to it, takes image from image list to change the background, sets its coordinates and dimensions, sets self.locked bullion value as True to indicate that the lock object has not been unlocked
+		ParamList: self (self), index (int), imagefile (string), coordinates (tuple), size (list)
+		Returns: none
+		"""
 		self.id = index
 		self.image = imagefile
 		self.top_x = coordinates[0]
@@ -9,6 +14,11 @@ class Lock:
 		self.locked = True
 
 	def isUnlocked(self):
+		"""
+		Changes self.locked bullion value to False to indicate that the lock object has been unlocked and makes it disappear from the view
+		ParamList: self (self)
+		Returns: none
+		"""
 		self.locked = False
 		self.top_x = 0
 		self.top_y = 0
